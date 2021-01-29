993a55d1210271c05cc6625868e1a36e355a72c3
Saurabh Mane <saurabhmane@Saurabhs-MacBook-Pro.local>
2021-01-29 15:05:21 +0530

    initial update

--- main.py
+++ main.py
@@ -24,31 +24,34 @@ while(True):
     print('Player ',player,' turn: ')
     MoveColumn = int(input("Enter column\n"))
     MoveRow = int(input("Enter row\n"))
-    if player == 1:
-        if currentField[MoveColumn][MoveRow]==' ':
-            currentField[MoveColumn][MoveRow]='X'
-            if(MoveColumn==MoveRow):
-                if(currentField[0][0]=='X' and currentField[1][1]=='X' and currentField[2][2]=='X'):
+    if (MoveColumn<=3 and MoveColumn>=1) and (MoveRow<=3 and MoveRow>=1):
+        if player == 1:
+            if currentField[MoveColumn-1][MoveRow-1]==' ':
+                currentField[MoveColumn-1][MoveRow-1]='X'
+                if(MoveColumn==MoveRow):
+                    if(currentField[0][0]=='X' and currentField[1][1]=='X' and currentField[2][2]=='X'):
+                        gameEnd=True
+                        won=1
+                if(currentField[MoveColumn][0]=='X' and currentField[MoveColumn][1]=='X' and currentField[MoveColumn][2]=='X'):
                     gameEnd=True
                     won=1
-            if(currentField[MoveColumn][0]=='X' and currentField[MoveColumn][1]=='X' and currentField[MoveColumn][2]=='X'):
-                gameEnd=True
-                won=1
-            player = 2
-    else:
-        if currentField[MoveColumn][MoveRow]==' ':
-            currentField[MoveColumn][MoveRow]='O'
-            if(MoveColumn==MoveRow):
-                if(currentField[0][0]=='O' and currentField[1][1]=='O' and currentField[2][2]=='O'):
+                player = 2
+        else:
+            if currentField[MoveColumn-1][MoveRow-1]==' ':
+                currentField[MoveColumn-1][MoveRow-1]='O'
+                if(MoveColumn==MoveRow):
+                    if(currentField[0][0]=='O' and currentField[1][1]=='O' and currentField[2][2]=='O'):
+                        gameEnd=True
+                        won=2
+                if(currentField[MoveColumn][0]=='O' and currentField[MoveColumn][1]=='O' and currentField[MoveColumn][2]=='O'):               
                     gameEnd=True
                     won=2
-            if(currentField[MoveColumn][0]=='O' and currentField[MoveColumn][1]=='O' and currentField[MoveColumn][2]=='O'):               
-                gameEnd=True
-                won=2
-            player = 1
-    print('\n')
-    drawField(currentField)
-    if gameEnd:
+                player = 1
         print('\n')
-        print('Player ',won,' won ! Game ends !')
-        break
+        drawField(currentField)
+        if gameEnd:
+            print('\n')
+            print('Player ',won,' won ! Game ends !')
+            break
+    else:
+        print("Invalid row or column! Please try again!")
